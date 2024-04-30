0467ec594df970b17a50e6ff39cadde5ed4748e1
GabContreras <contrerasgab01@gmail.com>
2024-04-30 15:42:30 -0600

    creación del proyecto

---
+++ app/src/main/java/gabriel/contreras/laboratoriopracticogabrielcontreras/Login.kt
@@ -0,0 +1,28 @@
+package gabriel.contreras.laboratoriopracticogabrielcontreras
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnIniciarSesion = findViewById<Button>(R.id.btnIniciarSesion)
+
+        btnIniciarSesion.setOnClickListener {
+            val pantalla2 = Intent(this,MainActivity::class.java)
+            startActivity(pantalla2)
+        }
+    }
+}
